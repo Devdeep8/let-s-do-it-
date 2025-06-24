a756ccc72434f2c34440cf55f93b431b08c6fc02
Devdeep Patidar <119996069+Devdeep8@users.noreply.github.com>
2025-06-24 18:06:51 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Deva',
+  description: 'Time to shine in the Dark',
 }
 
 export default function RootLayout({
